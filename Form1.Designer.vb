af56dad3271506d8a154cedf6d68685114b98dd7
myhillman <marc.hillman1@bigpond.com>
2025-01-15 15:04:09 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        OpenToolStripMenuItem = New ToolStripMenuItem()
+        OpenToolStripMenuItem1 = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        TextBox1 = New TextBox()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {OpenToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(1047, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' OpenToolStripMenuItem
+        ' 
+        OpenToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {OpenToolStripMenuItem1, ExitToolStripMenuItem})
+        OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        OpenToolStripMenuItem.Size = New Size(37, 20)
+        OpenToolStripMenuItem.Text = "File"
+        ' 
+        ' OpenToolStripMenuItem1
+        ' 
+        OpenToolStripMenuItem1.Name = "OpenToolStripMenuItem1"
+        OpenToolStripMenuItem1.Size = New Size(103, 22)
+        OpenToolStripMenuItem1.Text = "Open"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(103, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(10, 31)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ScrollBars = ScrollBars.Both
+        TextBox1.Size = New Size(1025, 565)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1047, 608)
+        Controls.Add(TextBox1)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "MOL"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,158 @@
+﻿Public Class Form1
+    Const BLOCK_SIZE = 512
+    Const STEP_SIZE = 208.33      ' steps/mm
+    Dim data() As Byte
+    Dim index As Integer = 0      ' index into data
+    Dim filesize As Integer
+    Private Sub OpenToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem1.Click
+        Dim dlg = New OpenFileDialog
+        With dlg
+            .Filter = "MOL file|*.mol|All files|*.*"
+        End With
+        Dim result = dlg.ShowDialog()
+        If result.OK Then
+            data = My.Computer.FileSystem.ReadAllBytes(dlg.FileName)
+            DisplayHeader()
+            DisplayConfig()
+            DisplayTest()
+            DisplayCut()
+            DisplayDraw()
+        End If
+
+    End Sub
+    Function getint() As Integer
+        ' read 4 byte integer from data(index)
+        Dim result As Integer
+        result = BitConverter.ToInt32({data(index), data(index + 1), data(index + 2), data(index + 3)})
+        index += 4      ' increment to next int
+        Return result
+    End Function
+    Function getint(n As Integer) As Integer
+        index = n
+        Return getint()
+    End Function
+
+    Function getfloat() As Single
+        ' read 4 byte float from data
+        ' Leetro float format  [eeeeeeee|smmmmmmm|mmmmmmm0|00000000]
+        ' TODO: needs to be tested
+        Dim b = getint()   ' get the float
+        Dim vExp As Integer = b >> 24 And &HFF      ' exponent
+        Dim vMan As Integer = b >> 9 And &H7FFF     ' mantissa
+        Dim vSgn As Integer = b >> 23 And 1         ' sign
+        If vSgn > 0 Then vSgn = -1 Else vSgn = 1
+        Return (vMan / 16384.0) * Math.Pow(2, vExp) * vSgn
+    End Function
+    Sub DisplayHeader()
+        ' Display header info
+        TextBox1.AppendText($"HEADER &0{vbCrLf}")
+        filesize = getint()
+        TextBox1.AppendText($"Size of file: &h{filesize:x}{vbCrLf}")
+        TextBox1.AppendText($"Version: {data(11)}.{data(10)}.{data(9)}.{data(8)}{vbCrLf}")
+        TextBox1.AppendText($"Origin: X {getint(&H18)} ({getint(&H18) / STEP_SIZE:f1}mm), Y {getint(&H1C)} ({getint(&H1C) / STEP_SIZE:f1}mm){vbCrLf}")
+        TextBox1.AppendText($"Bottom Left: X {getint(&H20)} ({getint(&H20) / STEP_SIZE:f1}mm), Y {getint(&H24)} ({getint(&H24) / STEP_SIZE:f1}mm){vbCrLf}")
+        TextBox1.AppendText($"Config chunk: {getint(&H70)}{vbCrLf}")
+        TextBox1.AppendText($"Test chunk: {getint(&H74)}{vbCrLf}")
+        TextBox1.AppendText($"Cut chunk: {getint(&H78)}{vbCrLf}")
+        TextBox1.AppendText($"Draw chunk: {getint(&H7C)}{vbCrLf}")
+        TextBox1.AppendText($"{vbCrLf}")
+    End Sub
+
+    Sub DisplayConfig()
+        ' Display config block info
+        Dim StartOfBlock = getint(&H70) * BLOCK_SIZE
+        TextBox1.AppendText($"CONFIG @&{StartOfBlock:x}{vbCrLf}")
+        index = StartOfBlock
+        For i = 1 To 8
+            TextBox1.AppendText($"Item {Index:x}: {getint():x}{vbCrLf}")
+        Next
+        TextBox1.AppendText($"{vbCrLf}")
+    End Sub
+    Sub DisplayTest()
+        ' Display test block info
+        Dim StartOfBlock = getint(&H74) * BLOCK_SIZE
+        TextBox1.AppendText($"TEST @&{StartOfBlock:x}{vbCrLf}")
+        HexDumpBlock(StartOfBlock)
+        TextBox1.AppendText($"{vbCrLf}")
+    End Sub
+
+    Sub DisplayCut()
+        ' Display cut block info
+        Dim StartOfBlock = getint(&H78) * BLOCK_SIZE
+        If StartOfBlock > 0 Then
+            TextBox1.AppendText($"CUT @&{StartOfBlock:x}{vbCrLf}")
+            HexDumpBlock(StartOfBlock)
+            TextBox1.AppendText($"{vbCrLf}")
+        End If
+    End Sub
+    Sub DisplayDraw()
+        ' Display cut block info
+        Dim StartOfBlock = getint(&H7C) * BLOCK_SIZE
+        TextBox1.AppendText($"DRAW @&{StartOfBlock:x}{vbCrLf}")
+        HexDumpBlock(StartOfBlock)
+        Dim done As Boolean = False
+        Dim LaserIsOn As Boolean = False
+        Dim spdMin As Single = 100
+        Dim spdMax As Single = 100
+        Dim px As Integer = 0
+        Dim py As Integer = 0
+        Dim xyScale As Single = 208.33
+
+        index = StartOfBlock
+        While Not done
+            Dim cmd = getint()
+            TextBox1.AppendText($"{index - 4:x} CMD {cmd}{vbCrLf}")
+            Select Case cmd
+                Case &H80000946 ' begin motion block
+                    getint()
+                    TextBox1.AppendText($"Begin motion block{vbCrLf}")
+                Case &H1000606     ' switch laser
+                    LaserIsOn = getint()
+                    TextBox1.AppendText($"Switching laser {LaserIsOn}{vbCrLf}")
+                Case &H3000301 ' set speeds
+                    spdMin = getint() / xyScale
+                    spdMax = getint() / xyScale
+                    getfloat() ' acceleration
+                    TextBox1.AppendText($"Set speed Min={spdMin}, Max={spdMax}{vbCrLf}")
+                Case &H3026000     ' move relative
+                    Dim axis = getint()
+                    Dim dx = getint()
+                    Dim dy = getint()
+                    If LaserIsOn Then
+                        ' Now draw 
+                        TextBox1.AppendText($"Moving relative dx={dx}, dy={dy}{vbCrLf}")
+                    End If
+                    px += dx
+                    py += dy
+                Case &H1400048, &H0
+                    done = True
+                Case Else
+                    Dim nWords = cmd >> 24
+                    If nWords = &H80 Or nWords = -128 Then
+                        nWords = getint()
+                        TextBox1.AppendText($"Skipping {nWords} words at {index:x}{vbCrLf}")
+                        index += 4 * nWords
+                    End If
+            End Select
+        End While
+        TextBox1.AppendText($"{vbCrLf}")
+    End Sub
+
+    Sub HexDumpBlock(addr As Integer)
+        ' dump a block in hex
+        Dim done As Boolean = False
+        index = addr
+        While Not done And index <= filesize
+            TextBox1.AppendText($"&h{index:x8}: ")
+            Dim cmd As Integer = getint()
+            If cmd = 0 Then Exit While
+            TextBox1.AppendText($"&h{cmd:x8}")
+            Dim nWords = cmd >> 24 And &H7F
+            For i = 1 To nWords
+                cmd = getint()
+                TextBox1.AppendText($" &h{cmd:x8}")
+            Next
+            TextBox1.AppendText($"{vbCrLf}")
+        End While
+    End Sub
+End Class
